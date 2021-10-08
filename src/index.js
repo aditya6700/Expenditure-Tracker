55e9355a0d6bc84d23fadc43f1d2db0c976e0b71
Sai Aditya Devulapalli <dsaiaditya@gmail.com>
2021-10-08 16:39:10 +0530

    tested basic functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const route = require('./routes/routes');
+const path = require('path');
+const hbs = require('hbs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // using available port
 const app = express();
 
-app.use(route);
+// setting path for css and templates
+const staticPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname,"../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
+
+// setting path for bootstrap files
+app.use('/css', express.static(path.join(__dirname,'../node_modules/bootstrap/dist/css/')));
+app.use('/js', express.static(path.join(__dirname,'../node_modules/bootstrap/dist/js/')));
+app.use('/jq', express.static(path.join(__dirname,'../node_modules/jquery/dist/')));
+
+app.use(express.static(staticPath));
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
+app.use(route); // using routes coming from routes.js
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-});
+}); 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 const express= require("express");
-
-
 const route = new express.Router();
 
 
 route.get('/', (req, res) => {
-    res.send("<h1>Welcome to Project Expenditure</h1>")
+    res.status(200).render('index');
+});
+
+route.get('*', (req, res) => {
+    res.status(404).render('error', {
+        errormsg: "The requested resource could not be found."
+    });
 });
 
 module.exports = route;
