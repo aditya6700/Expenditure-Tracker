46b21f2a6664528979c14c76a7fe53087e6868d3
aditya6700 <dsaiaditya@gmail.com>
2021-11-14 12:33:30 +0530

    report completed

--- public/js/statement.js
+++ public/js/statement.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('stat');
 const tableBody = document.getElementById('tableBody');
+const reportBody = document.getElementById('report');
 
 const updateTable = (data) => {
     
@@ -28,11 +29,107 @@ const updateTable = (data) => {
     tableBody.innerHTML = tableContent;
 };
 
+const updateReport = (reportObj) => {
+    reportContent = '';
+
+    for (const name in reportObj) {
+        if (Object.hasOwnProperty.call(reportObj, name)) {
+            reportContent += `<p class="mb-0"><strong>you have spent ${reportObj[name].total} for ${name}</strong></p>
+            <ul class="list-unstyled">`
+            let subObj = reportObj[name];
+            for (const payType in subObj) {
+                if (Object.keys(subObj[payType]).length !== 0 && subObj[payType].constructor === Object) {
+                    reportContent += `<li>using ${payType}:
+                    <ul>`
+                    let anotherSubObj = subObj[payType];
+                    for (const reason in anotherSubObj) {
+                        reportContent += `<li>${anotherSubObj[reason]} for ${reason}</li>`;
+                    };
+                    reportContent += `</ul></li>`;
+                };
+            };
+            reportContent+=`</ul>`
+        
+        };
+
+    };
+    reportBody.innerHTML = reportContent;
+}
+
+
+const getReport = (testData) => {
+    
+    var reportObj = {},
+        amountReportObj = {};
+
+    testData.forEach((ele) => {
+        let type = (ele.paidTo == "others") ? ele.name : "self"
+  
+        type = type.charAt(0).toUpperCase() + type.slice(1);
+        if (!reportObj[type]) {
+            reportObj[type] = new Array()
+        }
+        reportObj[type].push(ele);
+
+    });
+
+    // selfArr.forEach(ele => console.log(ele, "\n ---------------"));
+    // for (const key in reportObj) {
+    //     if (Object.hasOwnProperty.call(reportObj, key)) {
+    //         console.log(reportObj[key], "\n ---------------\n");
+        
+    //     }
+    // }
+
+    const reportFun = (obj, type) => {
+        amountReportObj[type] = {
+            "total": 0,
+            "GPay": {},
+            "PhonePay": {},
+            "PayTm": {},
+            "Cash": {},
+        };
+        // console.log(obj)
+
+        obj.forEach((ele) => {
+            amountReportObj[type]["total"] += +ele.amount;
+            (!amountReportObj[type][ele.paymentType][ele.reason]) ?
+                amountReportObj[type][ele.paymentType][ele.reason] = +ele.amount :
+                amountReportObj[type][ele.paymentType][ele.reason] += +ele.amount;
+        });
+
+    };
+
+    // reportFun(reportObj["self"],"self");
+
+    for (const key in reportObj) {
+        if (Object.hasOwnProperty.call(reportObj, key)) {
+            // console.log(reportObj[key], "\n ---------------\n");
+            reportFun(reportObj[key], key)
+        
+        }
+    }
+    // console.log(amountReportObj);
+
+    updateReport(amountReportObj);
+
+    // reportBody.innerHTML = JSON.stringify(amountReportObj);
+};
+
+
 const statememtDecode = async () => {
     try {
+        
+        let fromDate = document.getElementById('fromDate').value;
+        let toDate = document.getElementById('toDate').value;
+        if (!(toDate)) {
+            let date = new Date(fromDate);
+            date.setDate(date.getDate() + 1);
+            toDate = date;
+        }
 
-        const toDate = document.getElementById('toDate').value;
-        const fromDate = document.getElementById('fromDate').value;
+        // console.log(fromDate, toDate)
+       
 
         const statement = await fetch(`http://localhost:3000/query?toDate=${toDate}&fromDate=${fromDate}`, {
             method: 'GET',
@@ -44,9 +141,10 @@ const statememtDecode = async () => {
         const jsonStatement = await statement.json()
         const arrStatement = [jsonStatement][0];
 
-        console.log(arrStatement);
+        // console.log(arrStatement);
 
         updateTable(arrStatement);
+        getReport(arrStatement);
     }
     catch (error) {
         console.log("error occured" + error); 
